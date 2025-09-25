626a88c2bd76d075e1347924a1c89f5c4c877f27
Mikhailov Dmitry <eternal.martyr13579@gmail.com>
2025-09-25 08:55:39 +0300

    add dataset

---
+++ Lab1_ais/code/src.pl
@@ -0,0 +1,37 @@
+parent(nikolay, andrew).
+parent(tamara, andrew).
+parent(nikolay, larisa).
+parent(tamara, larisa).
+parent(larisa, denis).
+parent(larisa, angelina).
+
+parent(georgiy, galina).
+parent(agafya, galina).
+parent(georgiy, tatiana).
+parent(agafya, tatiana).
+parent(georgiy, vladimir).
+parent(agafya, vladimir).
+
+parent(boris, mama).
+parent(galina, mama).
+
+parent(andrew, dmitriy).
+parent(mama, dmitriy).
+
+parent(vyacheslav, ludmila).
+parent(tatiana, ludmila).
+parent(vyacheslav, ruslan).
+parent(tatiana, ruslan).
+
+parent(alexandr, anton).
+parent(ludmila, anton).
+parent(alexandr, eduard).
+parent(ludmila, eduard).
+parent(alexandr, madlen).
+parent(ludmila, madlen).
+
+parent(anton, artem).
+parent(yana, artem).
+
+parent(ruslan, diana).
+parent(ruslan, vika).
