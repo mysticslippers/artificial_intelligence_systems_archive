a8d12de1b1bb260f1ed92542bc2fd6dbbf5d6e7f
Mikhailov Dmitry <eternal.martyr13579@gmail.com>
2025-09-25 18:20:21 +0300

    add some other facts

--- Lab1_ais/code/src.pl
+++ Lab1_ais/code/src.pl
@@ -1,3 +1,5 @@
+% Существующие родственные связи.
+% -------------------------------------------------------------------
 parent(nikolay, andrew).
 parent(tamara, andrew).
 parent(nikolay, larisa).
@@ -35,3 +37,46 @@ parent(yana, artem).
 
 parent(ruslan, diana).
 parent(ruslan, vika).
+
+% События регистрации ЗАГС.
+% -------------------------------------------------------------------
+born(dmitriy, 2004).
+born(denis, 2007).
+born(angelina, 2018).
+born(larisa, 1975).
+born(andrew, 1980).
+born(mama, 1981).
+born(nikolay, 1950).
+born(tamara, 1953).
+born(georgiy, 1913).
+born(agafya, 1915).
+born(tatiana, 1941).
+born(vyacheslav, 1948).
+born(galina, 1943).
+born(vladimir, 1939).
+born(ludmila, 1977).
+born(alexandr, 1975).
+born(anton, 1996).
+born(eduard, 2001).
+born(madlen, 2007).
+
+married(nikolay, tamara, 2005).
+married(georgiy, agafya, 1930).
+married(boris, galina, 1998).
+married(andrew, mama, 2003).
+married(vyacheslav, tatiana, 1980).
+married(alexandr, ludmila, 1992).
+married(anton, yana, 2023).
+
+divorced(anton, yana, 2025).
+divorced(georgiy, agafya, 1990).
+divorced(andrew, mama, 2007).
+divorced(alexandr, ludmila, 2018).
+
+died(georgiy, 2006).
+died(nikolay, 2023).
+died(vladimir, 2010).
+died(agafya, 2013).
+
+% Правила.
+% -------------------------------------------------------------------
