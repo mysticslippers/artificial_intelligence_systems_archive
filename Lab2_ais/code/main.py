97a1fd80960e5e3d0b8ad3a3f408c1d87c9aae45
Mikhailov Dmitry <eternal.martyr13579@gmail.com>
2025-10-11 14:47:38 +0300

    update main.py

--- Lab2_ais/code/main.py
+++ Lab2_ais/code/main.py
@@ -1,3 +1,4 @@
+from ui.UserInputManager import UserInputManager
 from utils.MovieSearcher import MovieSearcher
 from utils.OntologyManager import OntologyManager
 
@@ -6,3 +7,9 @@ FILEPATH = "resources/ontology.rdf"
 if __name__ == "__main__":
     ontology_manager = OntologyManager(FILEPATH)
     movie_searcher = MovieSearcher(ontology_manager.graph, ontology_manager.namespace, ontology_manager.get_by_type("Movie"))
+    userInputManager = UserInputManager()
+    userInputManager.parse("Мне 24 года. Мне нравятся жанры: Fantasy, Action; страна производства: USA; с рейтинг: 6.0; с годом производства: 2001; с название: Pulp Fiction; с режиссёр: Christopher Nolan; с актёр: Cillian Murphy; с кинокомпания: A Band Apart")
+    dictionary = userInputManager.get_criteria()
+    keys = dictionary.keys()
+    for i in keys:
+        print(i, dictionary.get(i))
