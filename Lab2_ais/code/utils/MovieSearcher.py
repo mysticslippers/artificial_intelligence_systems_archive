096b853a26e7333de05188b64c1b17734dc7a2d7
Mikhailov Dmitry <eternal.martyr13579@gmail.com>
2025-10-12 12:36:45 +0300

    fix MovieSearcher.py (final version)

--- Lab2_ais/code/utils/MovieSearcher.py
+++ Lab2_ais/code/utils/MovieSearcher.py
@@ -9,6 +9,9 @@ class MovieSearcher:
         self.namespace = namespace
         self.collection = collection
 
+    def _uri(self, s: str) -> URIRef:
+        return URIRef(str(self.namespace) + "".join(s.split()))
+
     def search_movies(self,
                       genres: Iterable[str] = None,
                       age_limit: int = None,
@@ -17,101 +20,105 @@ class MovieSearcher:
                       release_year: int = None,
                       title: str = None,
                       director_uri: str = None,
-                      actor_uri: str = None) -> List[URIRef]:
-        result: List[URIRef] = []
-
+                      actor_uri: str = None,
+                      production_company_uri: str = None) -> List[URIRef]:
+        result_set = set(self.collection)
 
         if genres is not None:
-            filtered = list()
-            genres_uris = {URIRef(str(self.namespace) + str(genre)) for genre in genres}
-            for movie in self.collection:
+            required_genres = {URIRef(str(self.namespace) + str(genre).strip()) for genre in genres}
+            filtered = set()
+            for movie in result_set:
                 movie_genres = set(self.graph.objects(movie, self.namespace.hasGenreIn))
-                if genres_uris.issubset(movie_genres):
-                    filtered.append(movie)
-            result = filtered
+                if required_genres.issubset(movie_genres):
+                    filtered.add(movie)
+            result_set = filtered
 
         if age_limit is not None:
-            filtered = list()
-            for movie in self.collection:
+            filtered = set()
+            for movie in result_set:
                 for al in self.graph.objects(movie, self.namespace.hasAgeLimit):
                     try:
                         if int(al) <= int(age_limit):
-                            filtered.append(movie)
+                            filtered.add(movie)
                             break
                     except (TypeError, ValueError):
                         continue
-            result = filtered
+            result_set = filtered
 
         if country is not None:
-            filtered = list()
             tmp = country.lower()
-            for movie in self.collection:
+            filtered = set()
+            for movie in result_set:
                 for movie_country in self.graph.objects(movie, self.namespace.hasCountry):
-                    if isinstance(movie_country, str):
-                        country_str = movie_country.lower()
-                    else:
-                        country_str = str(movie_country).lower()
+                    country_str = str(movie_country).lower() if not isinstance(movie_country, str) else movie_country.lower()
                     if tmp in country_str:
-                        filtered.append(movie)
+                        filtered.add(movie)
                         break
-            result = filtered
+            result_set = filtered
 
         if rating is not None:
-            filtered = list()
-            for movie in self.collection:
+            filtered = set()
+            for movie in result_set:
                 for r in self.graph.objects(movie, self.namespace.hasRating):
                     try:
                         if float(r) >= float(rating):
-                            filtered.append(movie)
+                            filtered.add(movie)
                             break
                     except (TypeError, ValueError):
                         continue
-            result = filtered
+            result_set = filtered
 
         if release_year is not None:
-            filtered = list()
-            for movie in self.collection:
+            filtered = set()
+            for movie in result_set:
                 for r_year in self.graph.objects(movie, self.namespace.hasReleaseYear):
                     try:
                         if int(r_year) == int(release_year):
-                            filtered.append(movie)
+                            filtered.add(movie)
                             break
                     except (TypeError, ValueError):
                         continue
-                result = filtered
+            result_set = filtered
 
         if title is not None:
-            filtered = list()
             tmp = title.lower()
-            for movie in self.collection:
+            filtered = set()
+            for movie in result_set:
                 for movie_title in self.graph.objects(movie, self.namespace.hasTitle):
-                    if isinstance(movie_title, str):
-                        title_str = movie_title.lower()
-                    else:
-                        title_str = str(movie_title).lower()
+                    title_str = str(movie_title).lower() if not isinstance(movie_title, str) else movie_title.lower()
                     if tmp in title_str:
-                        filtered.append(movie)
+                        filtered.add(movie)
                         break
-            result = filtered
+            result_set = filtered
 
         if director_uri is not None:
-            filtered = list()
-            director = URIRef(str(self.namespace) + "".join(director_uri.split()))
-            for movie in self.collection:
+            director = self._uri(director_uri)
+            filtered = set()
+            for movie in result_set:
                 for directed_in in self.graph.objects(director, self.namespace.hasDirectedIn):
                     if directed_in == movie:
-                        filtered.append(movie)
+                        filtered.add(movie)
                         break
-            result = filtered
+            result_set = filtered
 
         if actor_uri is not None:
-            filtered = list()
-            actor = URIRef(str(self.namespace) + "".join(actor_uri.split()))
-            for movie in self.collection:
+            actor = self._uri(actor_uri)
+            filtered = set()
+            for movie in result_set:
                 for acted_in in self.graph.objects(actor, self.namespace.hasActedIn):
                     if acted_in == movie:
-                        filtered.append(movie)
+                        filtered.add(movie)
                         break
-            result = filtered
+            result_set = filtered
 
-        return result
+        if production_company_uri is not None:
+            production_company = self._uri(production_company_uri)
+            filtered = set()
+            for movie in result_set:
+                for producedIn in self.graph.objects(production_company, self.namespace.hasProducedIn):
+                    if producedIn == movie:
+                        filtered.add(movie)
+                        break
+            result_set = filtered
+
+        return list(result_set)
