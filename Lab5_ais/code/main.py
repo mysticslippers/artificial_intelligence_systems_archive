1ddc8d5b81540fb751e541193443d72ac25260dd
Mikhailov Dmitry <eternal.martyr13579@gmail.com>
2025-11-15 18:16:27 +0300

    add fill_missing_with_mode function

--- Lab5_ais/code/main.py
+++ Lab5_ais/code/main.py
@@ -16,6 +16,28 @@ def select_random_features(data: pd.DataFrame, y_label: str) -> List[str]:
     return selected_features
 
 
+def fill_missing_with_mode(data: pd.DataFrame, feature_names: List[str], missing_value: str = "?") -> pd.DataFrame:
+    data = data.copy()
+
+    for col in feature_names:
+        null_idx = data.index[data[col] == missing_value]
+
+        if len(null_idx) == 0:
+            continue
+
+        mode_series = data.loc[data[col] != missing_value, col].mode()
+        if mode_series.empty:
+            continue
+
+        mode_value = mode_series.iloc[0]
+        n_nulls = len(null_idx)
+
+        data.loc[null_idx, col] = mode_value
+        print(f"{n_nulls} null values in {col} changed to {mode_value}")
+
+    return data
+
+
 def main(path: str = "AgaricusLepiota.csv") -> None:
     try:
         data = load_data(path)
